b1b871d3e115a5b7d6c7e26997be029ef956bd12
youcyousyunn <731781984@qq.com>
2020-09-29 11:50:17 +0800

    认证服务更新

--- gulimall-auth/src/main/java/com/ycs/gulimall/controller/LoginController.java
+++ gulimall-auth/src/main/java/com/ycs/gulimall/controller/LoginController.java
@@ -12,12 +12,12 @@ import com.ycs.gulimall.vo.UserRegisterVo;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
@@ -30,7 +30,7 @@ import java.util.stream.Collectors;
 
 import static com.ycs.gulimall.constant.AuthConstant.LOGIN_USER;
 
-@RestController
+@Controller
 public class LoginController {
     @Autowired
     private ThirdPartFeignService thirdPartFeignService;
