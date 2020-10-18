e4fe5c9b25cf24ac472e9bc18369631013111625
youcyousyunn <731781984@qq.com>
2020-10-18 21:56:37 +0800

    修改会员收货地址控制器

--- gulimall-member/src/main/java/com/ycs/gulimall/controller/MemberReceiveAddressController.java
+++ gulimall-member/src/main/java/com/ycs/gulimall/controller/MemberReceiveAddressController.java
@@ -36,7 +36,6 @@ public class MemberReceiveAddressController {
      * 列表
      */
     @RequestMapping("/list")
-    //@RequiresPermissions("member:memberreceiveaddress:list")
     public R list(@RequestParam Map<String, Object> params){
         PageUtils page = memberReceiveAddressService.queryPage(params);
         return R.ok().put("page", page);
@@ -47,7 +46,6 @@ public class MemberReceiveAddressController {
      * 信息
      */
     @RequestMapping("/info/{id}")
-    //@RequiresPermissions("member:memberreceiveaddress:info")
     public R info(@PathVariable("id") Long id){
 		MemberReceiveAddressEntity memberReceiveAddress = memberReceiveAddressService.getById(id);
         return R.ok().put("memberReceiveAddress", memberReceiveAddress);
@@ -57,7 +55,6 @@ public class MemberReceiveAddressController {
      * 保存
      */
     @RequestMapping("/save")
-    //@RequiresPermissions("member:memberreceiveaddress:save")
     public R save(@RequestBody MemberReceiveAddressEntity memberReceiveAddress){
 		memberReceiveAddressService.save(memberReceiveAddress);
         return R.ok();
@@ -67,7 +64,6 @@ public class MemberReceiveAddressController {
      * 修改
      */
     @RequestMapping("/update")
-    //@RequiresPermissions("member:memberreceiveaddress:update")
     public R update(@RequestBody MemberReceiveAddressEntity memberReceiveAddress){
 		memberReceiveAddressService.updateById(memberReceiveAddress);
         return R.ok();
@@ -77,7 +73,6 @@ public class MemberReceiveAddressController {
      * 删除
      */
     @RequestMapping("/delete")
-    //@RequiresPermissions("member:memberreceiveaddress:delete")
     public R delete(@RequestBody Long[] ids){
 		memberReceiveAddressService.removeByIds(Arrays.asList(ids));
         return R.ok();
