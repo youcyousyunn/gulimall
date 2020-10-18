0c1f4d77ad82e4da78284b981c37b51b283171dc
youcyousyunn <731781984@qq.com>
2020-10-18 21:54:32 +0800

    更新公共服务异常枚举

--- gulimall-common/src/main/java/com/ycs/gulimall/exception/BizCodeEnum.java
+++ gulimall-common/src/main/java/com/ycs/gulimall/exception/BizCodeEnum.java
@@ -13,8 +13,9 @@ package com.ycs.gulimall.exception;
  *  11: 商品
  *  12: 订单
  *  13: 购物车
- *  14: 物流
- *  15：用户
+ *  14: 库存
+ *  15: 用户
+ *  16: 物流
  **/
 public enum BizCodeEnum {
     UNKNOW_EXCEPTION(10000,"系统未知异常"),
@@ -22,9 +23,11 @@ public enum BizCodeEnum {
     TO_MANY_REQUEST(10002,"请求流量过大，请稍后再试"),
     SMS_CODE_EXCEPTION(10002,"验证码获取频率太高，请稍后再试"),
     PRODUCT_UP_EXCEPTION(11000,"商品上架异常"),
+    ORDER_TOKEN_EXPIRED(12001,"订单令牌信息过期，请刷新页面再次提交"),
+    ORDER_PRODUCT_PRICE_CHANGED(12002,"订单商品价格发生变化，请确认后再次提交"),
+    NO_STOCK_EXCEPTION(14001,"商品库存不足"),
     USER_EXIST_EXCEPTION(15001,"存在相同的用户"),
     PHONE_EXIST_EXCEPTION(15002,"存在相同的手机号"),
-    NO_STOCK_EXCEPTION(21000,"商品库存不足"),
     LOGIN_ACCT_PASSWORD_EXCEPTION(15003,"账号或密码错误"),
     ;
 
