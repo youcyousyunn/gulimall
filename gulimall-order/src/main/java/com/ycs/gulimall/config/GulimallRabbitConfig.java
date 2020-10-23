8e4236aa234acff84786443a8442ab4c5fdcc9c0
youcyousyunn <731781984@qq.com>
2020-10-23 16:39:00 +0800

    重命名订单服务RabbitConfig配置类

--- gulimall-order/src/main/java/com/ycs/gulimall/config/GulimallRabbitConfig.java
+++ gulimall-order/src/main/java/com/ycs/gulimall/config/GulimallRabbitConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Primary;
 
 @Slf4j
 @Configuration
-public class MyRabbitConfig {
+public class GulimallRabbitConfig {
     private RabbitTemplate rabbitTemplate;
 
 
@@ -20,8 +20,8 @@ public class MyRabbitConfig {
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         this.rabbitTemplate = rabbitTemplate;
-        rabbitTemplate.setMessageConverter(messageConverter());
-        initRabbitTemplate();
+        rabbitTemplate.setMessageConverter(this.messageConverter());
+        this.initRabbitTemplate();
         return rabbitTemplate;
     }
 
@@ -32,9 +32,9 @@ public class MyRabbitConfig {
 
     /**
      * 定制RabbitTemplate
-     * 1、服务收到消息就会回调
+     * 1、消息代理服务器收到消息就会进行回调
      *      1、spring.rabbitmq.publisher-confirms: true
-     *      2、设置确认回调
+     *      2、设置确认回调ConfirmCallback
      * 2、消息正确抵达队列就会进行回调
      *      1、spring.rabbitmq.publisher-returns: true
      *         spring.rabbitmq.template.mandatory: true
@@ -43,19 +43,18 @@ public class MyRabbitConfig {
      * 3、消费端确认(保证每个消息都被正确消费，此时才可以broker删除这个消息)
      *
      */
-    // @PostConstruct  //MyRabbitConfig对象创建完成以后，执行这个方法
+    // @PostConstruct  //GulimallRabbitConfig对象创建完成以后，执行这个方法
     public void initRabbitTemplate() {
         /**
-         * 1、只要消息抵达Broker就ack=true
+         * 1、只要消息抵达消息代理服务器-Broker就ack=true
          * correlationData：当前消息的唯一关联数据(这个是消息的唯一id)
          * ack：消息是否成功收到
          * cause：失败的原因
          */
         //设置确认回调
         rabbitTemplate.setConfirmCallback((correlationData,ack,cause) -> {
-            log.info("confirm...correlationData["+correlationData+"]==>ack:["+ack+"]==>cause:["+cause+"]");
+            log.info("接收消息内容["+correlationData+"]==>消息是否成功收到:["+ack+"]==>失败的原因:["+cause+"]");
         });
-
 
         /**
          * 只要消息没有投递给指定的队列，就触发这个失败回调
@@ -63,11 +62,11 @@ public class MyRabbitConfig {
          * replyCode：回复的状态码
          * replyText：回复的文本内容
          * exchange：当时这个消息发给哪个交换机
-         * routingKey：当时这个消息用哪个路邮键
+         * routingKey：当时这个消息用哪个路由键
          */
         rabbitTemplate.setReturnCallback((message,replyCode,replyText,exchange,routingKey) -> {
-            log.info("Fail Message["+message+"]==>replyCode["+replyCode+"]" +
-                    "==>replyText["+replyText+"]==>exchange["+exchange+"]==>routingKey["+routingKey+"]");
+            log.info("投递失败的消息详细信息["+message+"]==>回复的状态码["+replyCode+"]" +
+                    "==>回复的文本内容["+replyText+"]==>当时这个消息发给哪个交换机["+exchange+"]==>当时这个消息用哪个路由键["+routingKey+"]");
         });
     }
 }
